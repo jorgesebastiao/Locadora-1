b4a400df03eb4737650117b0ef1607cff7ce21f0
Leonardo <leonardomagalhaes.leo@gmail.com>
2020-09-16 21:54:05 -0300

    Alterado o frontend para pegar o endereço do backend dinamicamente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+let apiAddress = '';
+
+if (process.env.NODE_ENV === 'production') {
+    apiAddress = 'https://locadorawebapp.azurewebsites.net';
+} else {
+    apiAddress = 'http://localhost:5000';
+}
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: apiAddress,
 })
 
 export default api;
